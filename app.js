7977fb76d8144f336966d2c8e41b3eefd28facd7
Ggmann-37 <javiergarmoy@proton.me>
2026-04-15 20:17:17 +0200

    Fix syntax error in app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const SUPABASE_URL = 'https://ymqzdewfxzsygujluyiy.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_XVgNadNTBRQgX0hjozPO-Q_D8wWbS0P';
 
-}
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
